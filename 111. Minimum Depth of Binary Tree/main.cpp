e54f6dec9e67be62dcd25a18ac02d454ac8fc85d
PCzhangPC <ZhangPCplus@163.com>
2018-02-09 11:06:04 +0800

    finish 111
    Signed-off-by: PCzhangPC <ZhangPCplus@163.com>

---
+++ 111. Minimum Depth of Binary Tree/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        if(!root) return 0;
+        if(!root->left) return 1 + minDepth(root->right);
+        if(!root->right) return 1 + minDepth(root->left);
+        return 1+min(minDepth(root->left),minDepth(root->right));
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
