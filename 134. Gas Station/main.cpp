7fd1371bdf2ed4224f0638aaa4af858ffe71f4c7
PCzhangPC <ZhangPCplus@163.com>
2018-02-26 15:46:57 +0800

    finish 134
    Signed-off-by: PCzhangPC <ZhangPCplus@163.com>

---
+++ 134. Gas Station/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+
+        int start = gas.size()-1;
+        int end = 0;
+        int sum = gas[start] - cost[start];
+        while (start > end) {
+            if (sum >= 0) {
+                sum += gas[end] - cost[end];
+                ++end;
+            }
+            else {
+                --start;
+                sum += gas[start] - cost[start];
+            }
+        }
+        return sum >= 0 ? start : -1;
+    }
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
